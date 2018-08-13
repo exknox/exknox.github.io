90a9bb473808efb17eecd90e7e343d92c21a2b0a
Joshua Waiswa <jwaiswa7@gmail.com>
2018-08-13 20:43:29 +0300

    Taking out the mailing list

--- main.6cfdd60c6d0446e78031.js
+++ main.6cfdd60c6d0446e78031.js
@@ -6377,16 +6377,6 @@ return validator.normalizeEmail = function(email, options) {
                     }, "chat"))), React.createElement("li", {
                         className: "dropdown-item"
                     }, React.createElement("a", {
-                        href: "http://etherboost.us13.list-manage.com/subscribe?u=6ad46e524f84c79c4d8475e91&id=b7675dc8dd",
-                        target: "_blank"
-                    }, React.createElement("i", {
-                        className: "fa fa-envelope",
-                        "aria-hidden": "true"
-                    }), " ", React.createElement("span", {
-                        className: "trn"
-                    }, "mailing_list"))), React.createElement("li", {
-                        className: "dropdown-item"
-                    }, React.createElement("a", {
                         href: "https://www.reddit.com/r/EtherDelta/comments/6hrvwl/how_fees_work/",
                         target: "_blank"
                     }, React.createElement("i", {
